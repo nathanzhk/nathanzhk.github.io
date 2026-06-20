954ae0e43728ec7c365196db1b92efc06e057f32
Nathan <nathanzhk@gmail.com>
2026-06-19 19:14:12 -0700

    feat: add fumadocs ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { RootProvider } from 'fumadocs-ui/provider/next';
 import './globals.css';
 
 const geistSans = Geist({
@@ -25,9 +26,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <RootProvider>{children}</RootProvider>
+      </body>
     </html>
   );
 }
